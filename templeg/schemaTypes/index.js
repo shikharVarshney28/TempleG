4ce0c84e5717dc6545fe213c0d56951c23e3af32
Shikhar <shikharvarshney1008@gmail.com>
2026-01-15 09:39:51 +0530

    150626

--- templeg/schemaTypes/index.js
+++ templeg/schemaTypes/index.js
@@ -1,6 +1,6 @@
-import darshna from "./darshna";
+import darshan from "./darshan";
 import donation from "./donation";
 import event from "./event";
 import gallery from "./gallery";
 
-export const schemaTypes = [gallery,donation,event,darshna]
+export const schemaTypes = [gallery,donation,event,darshan]
